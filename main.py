c57549a8fed9e20dcaafa2df286bd69f87237622
umidjanovnaa <sheraliyevaruxshona08@gmail.com>
2023-12-02 02:37:25 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# #1-masala
+number = int(input("Son: "))
+if number % 2 == 0:
+    print("Rahmat!")
+else:
+    print("Bu juft son emas: ")
+#2-masala
+while True:
+        n = int(input("olma: "))
+        m = int(input("o`quvchilar: "))
+        print(n % m)
